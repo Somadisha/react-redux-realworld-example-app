bf8dc6bb7dc8709f59c96bdbbeff880ba68113fa
Prashanthp0703 <prashanthp0703@gmail.com>
2021-07-25 19:35:17 +0530

    added

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ import { routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory';
 
 export const history = createHistory();
-
+console.log("hello")
 // Build the middleware for intercepting and dispatching navigation actions
 const myRouterMiddleware = routerMiddleware(history);
 const getMiddleware = () => {
