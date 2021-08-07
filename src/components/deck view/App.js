a4ced87baf1de1fbf9a92756308539e701c55a54
vrishanka <55500396+vrishanka@users.noreply.github.com>
2021-08-07 16:20:37 +0530

    commit

---
+++ src/components/deck view/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SwipeCard from "./SwipeCard"
+
+
+function App() {
+  return (
+    <div className="App">
+      <SwipeCard/>
+
+    </div>
+  );
+}
+
+export default App;
